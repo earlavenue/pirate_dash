b0a0fcb4b475f826effa7ff2dbc3251fa7e5cd6b
Mike Knepper <michaelrknepper@gmail.com>
2013-06-28 16:10:16 -0500

    Moved import to top of people index. Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.new
+@user.email = 'pirate@example.com'
+@user.password = '12345'
+@user.password_confirmation = '12345'
+@user.save
+puts 'Log in as "pirate":'
+puts 'email: "pirate@example.com"'
+puts 'password: "12345"'
+puts ''
+puts 'If you are importing people from KISSmetrics, make sure the csv has only two columns: "km_userid" and "pedserial".'
