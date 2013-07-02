ba7c2e4c6c67c43f773c344d0b51b9d3d02ae069
Mike Knepper <michaelrknepper@gmail.com>
2013-07-02 13:34:25 -0500

    Added name to user created in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.destroy_all
-@user = User.new
-@user.email = 'pirate@example.com'
-@user.password = '12345'
-@user.password_confirmation = '12345'
-@user.save
-puts 'Log in as "pirate":'
+User.create(
+    {email: 'pirate@example.com',
+    first_name: 'Edward',
+    last_name: 'Teach',
+    password: '12345',
+    password_confirmation: '12345'})
+
+puts 'Log in as Edward Teach with these credentials:'
 puts 'email: "pirate@example.com"'
 puts 'password: "12345"'
 puts ''
