4b02c3fecc557c33940dc02fdfab80b163081942
Mike Knepper <michaelrknepper@gmail.com>
2013-07-02 10:47:02 -0500

    Added first/last name to Users. Cleaned up user index and show pages. Nav bar greets user's first name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest, :password, :password_confirmation
+  attr_accessible :email, :password_digest, :password, :password_confirmation, :first_name, :last_name
   has_secure_password
   validates :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702144911) do
+ActiveRecord::Schema.define(:version => 20130702154001) do
 
   create_table "events", :force => true do |t|
     t.string  "action"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(:version => 20130702144911) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
