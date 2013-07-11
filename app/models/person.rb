374fcc2bf350fe62c65514d58e064cc6d2d78bb0
Mike Knepper <michaelrknepper@gmail.com>
2013-07-11 14:42:57 -0500

    Cleanup: moved import functionality to operations view. Deleted some unnecessary methods

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,30 +12,11 @@ class Person < ActiveRecord::Base
       person.last_name = row_hash["User Name"].split(" ")[1]
       person.email = person.last_name + "@gmail.com"
       person.dev_serial = "HJ" + rand(9383838).to_s
-      person.organization = "Rush Hospital"
+      person.organization = "Rush Hospital" #eventually dynamic
         if Person.find_by_last_name(person.last_name)
         else
         person.save!
         end
-    end
-  end
-
-
-
-#import method supports importing data from EITHER KM or Omron production db. When you import from BOTH sources, the people table will have duplicates. Use the sync method to resolve this problem by identifying duplicates through identical device serial numbers, saving the KM id to the closer-to-complete Omron person row, and deleting the KM duplicate row.
-
-  def self.sync
-    people_needing_km_data = Person.where km_userid: nil
-    unsynced_km_people = Person.where("km_userid IS NOT NULL AND omron_userid IS NULL")
-
-    people_needing_km_data.each do |person_needing_km_userid|
-      unsynced_km_people.each do |person_from_km|
-        if person_needing_km_userid.dev_serial == person_from_km.dev_serial
-          person_needing_km_userid.km_userid = person_from_km.km_userid
-          person_needing_km_userid.save
-          person_from_km.destroy
-        end
-      end
     end
   end
 
@@ -46,13 +27,4 @@ class Person < ActiveRecord::Base
   def self.search_serial_results(query)
     Person.where dev_serial: query
   end
-
-  # def self.recently_active
-  #   recently_active_people = []
-  #   recent_events = Event.select("DISTINCT(PERSON_ID)").order("EVENT_TIME DESC").limit(10)
-  #   recent_events.each do |event|
-  #     recently_active_people << Person.find_by_id(event.person_id)
-  #   end
-  #   return recently_active_people
-  # end
 end
