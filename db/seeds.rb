4d9a933c7e30baf2d32789ef8b0f40750017f7e8
Mike Knepper <michaelrknepper@gmail.com>
2013-07-11 11:56:36 -0500

    Fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,65 +21,47 @@ Person.create(
     omron_userid: '86301',
     first_name: "Luke",
     last_name: "Skywalker",
-    email: "luke@example.com",
-    gender: "male",
-    city: "Tatooine",
-    state: "KS",
-    country: "USA"})
+    email: "luke@example.com"
+    })
 Person.create(
     {dev_serial: 'HJ72026071490',
     km_userid: '1002',
     omron_userid: '552',
     first_name: "Andrew",
     last_name: "Wiggin",
-    email: "andrew@example.com",
-    gender: "male",
-    city: "Milagre",
-    state: "FL",
-    country: "USA"})
+    email: "andrew@example.com"
+    })
 Person.create(
     {dev_serial: 'HJ72025088790',
     km_userid: '1012',
     omron_userid: '1985',
     first_name: "Marty",
     last_name: "McFly",
-    email: "marty@example.com",
-    gender: "male",
-    city: "Hill Valley",
-    state: "CA",
-    country: "USA"})
+    email: "marty@example.com"
+    })
 Person.create(
     {dev_serial: 'HJ72027007820',
     km_userid: '1075',
     omron_userid: '1998',
     first_name: "Michael",
     last_name: "Jordan",
-    email: "michael@example.com",
-    gender: "male",
-    city: "Chicago",
-    state: "IL",
-    country: "USA"})
+    email: "michael@example.com"
+    })
 Person.create(
     {dev_serial: 'HJ72012066850',
     km_userid: '1077',
     omron_userid: '1929084',
     first_name: "Audrey",
     last_name: "Hepburn",
-    email: "audrey@example.com",
-    gender: "female",
-    city: "Los Angeles",
-    state: "CA",
-    country: "USA"})
+    email: "audrey@example.com"
+    })
 Person.create(
     {dev_serial: 'HJ72026109690',
     km_userid: '1123',
     omron_userid: '628200',
     first_name: "Miles",
     last_name: "Davis",
-    email: "miles@example.com",
-    gender: "male",
-    city: "St. Louis",
-    state: "MO",
-    country: "USA"})
+    email: "miles@example.com"
+    })
 
 puts 'Six people created'
