327d6ce0fb3e36e26bd558923299a6a894ed7474
Mike Knepper <michaelrknepper@gmail.com>
2013-07-31 16:13:07 -0500

    Fixed tests for regular user on organizations controller spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe OrganizationsController do
 
+  before :each do
+    @organization = create(:organization)
+  end
+
+
   context 'guest visitor' do
-    before :each do
-      @organization = create(:organization)
-    end
 
     describe 'GET #index' do
       it "requires login" do
@@ -13,36 +15,42 @@ describe OrganizationsController do
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'GET #show' do
       it "requires login" do
         get :show, id: @organization
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'GET #new' do
       it "requires login" do
         get :new
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'GET #edit' do
       it "requires login" do
         get :edit, id: @organization
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'POST #create' do
       it "requires login" do
         post :create
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'PUT #update' do
       it "requires login" do
         put :update, id: @organization
         expect(response).to redirect_to new_session_url
       end
     end
+
     describe 'DELETE #destroy' do
       it "requires login" do
         delete :destroy, id: @organization
@@ -51,60 +59,66 @@ describe OrganizationsController do
     end
   end
 
-  # context 'regular user' do
-  #   before :each do
-  #     user = create(:user)
-  #     session[:user_id] = user.id
-  #   end
 
-  #   describe 'GET #index' do
-  #     it "requires omron" do
-  #       get :index
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
+  context 'regular user' do
+    before :each do
+      user = create(:user)
+      session[:user_id] = user.id
+    end
 
-  #   describe 'GET #show' do
-  #     it "requires omron" do
-  #       get :show, id: @organization
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  #   describe 'GET #new' do
-  #     it "requires omron" do
-  #       get :new
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  #   describe 'GET #edit' do
-  #     it "requires omron" do
-  #       get :edit, id: @organization
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  #   describe 'POST #create' do
-  #     it "requires omron" do
-  #       post :create
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  #   describe 'PUT #update' do
-  #     it "requires omron" do
-  #       put :update, id: @organization
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  #   describe 'DELETE #destroy' do
-  #     it "requires omron" do
-  #       delete :destroy, id: @organization
-  #       expect(response).to redirect_to people_url
-  #     end
-  #   end
-  # end
+    describe 'GET #index' do
+      it "requires omron" do
+        get :index
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'GET #show' do
+      it "requires omron" do
+        get :show, id: @organization
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'GET #new' do
+      it "requires omron" do
+        get :new
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'GET #edit' do
+      it "requires omron" do
+        get :edit, id: @organization
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'POST #create' do
+      it "requires omron" do
+        post :create
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'PUT #update' do
+      it "requires omron" do
+        put :update, id: @organization
+        expect(response).to redirect_to people_url
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it "requires omron" do
+        delete :destroy, id: @organization
+        expect(response).to redirect_to people_url
+      end
+    end
+  end
+
 
   context 'omron user' do
     before :each do
-      @organization = create(:organization)
       omron_user = create(:omron_user)
       session[:user_id] = omron_user.id
       @omron_organization = omron_user.organization
@@ -127,8 +141,6 @@ describe OrganizationsController do
         expect(assigns(:organization)).to eq @organization
       end
     end
-
-
   end
 
 end
