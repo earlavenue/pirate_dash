a1055e8f2f0bb4ad409878b42bf430bb0abe3856
Mike Knepper <michaelrknepper@gmail.com>
2013-07-31 13:56:32 -0500

    Work on User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe User do
   context "that exists" do
+    before :each do
+      @user = create(:user, first_name: "batman", password: "gotham")
+    end
+
     it "authenticates with matching username and password" do
-      user = create(:user, first_name: "batman", password: "secret")
-      user.authenticate("secret").should be_true#eq(user)
+      expect(@user.authenticate("gotham")).to be_valid
     end
 
     it "does not authenticate with incorrect password" do
-      user = create(:user, first_name: "batman", password: "secret")
-      user.authenticate("incorrect").should be_false
+      expect(@user.authenticate("metropolis")).to be_false
     end
+  end
+
+  it "has a unique email address" do
+    create(:user, email: "duke@ellington.com")
+    user = build(:user, email: "duke@ellington.com")
+    expect(user).to have(1).errors_on(:email)
   end
 end
