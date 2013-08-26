a9f03b3fb80ce9f953553c9dd7ab5e333ed2f868
Mike Knepper <michaelrknepper@gmail.com>
2013-08-26 15:08:58 -0500

    Wrote test for decoding secret strings

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -33,4 +33,22 @@ describe Membership do
     expect(Membership.set_first_upload_date(charlie)).to eq((Time.zone.now - 4.months).to_date)
   end
 
+  it "decodes an encrypted and encoded string" do
+    require 'openssl'
+    string = "Bonjour, mon amie!"
+    cipher = OpenSSL::Cipher::AES.new(256, :CBC)
+    cipher.encrypt
+    cipher.key = "x04xD4xA7xB4sTx12xF3x1Ax9DxD1xC9xA0Hx9Ex86x1Cf1x05VMnxDFMxA3xA9ixDCsxA6"
+    cipher.iv = "x11x8Dx02x1A[xCA'xF9xE8.JxADbx06x95x02"
+
+    encrypted_string = cipher.update(string) + cipher.final
+    encoded_and_encrypted = Base64.encode64(encrypted_string).encode('utf-8')
+
+    params = {}
+    params[:membership] = {}
+    params[:membership][:person_id] = encoded_and_encrypted
+
+    expect(Membership.decode_user_id(params)).to eq("Bonjour, mon amie!")
+  end
+
 end
