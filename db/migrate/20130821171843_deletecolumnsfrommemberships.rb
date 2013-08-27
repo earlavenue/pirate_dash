c75280962ed472857d41bbab10c72c34c0153d0a
Mike Knepper <michaelrknepper@gmail.com>
2013-08-27 14:42:06 -0500

    Deleted two migrations (only migration is blank production sync)

--- db/migrate/20130821171843_deletecolumnsfrommemberships.rb
+++
@@ -1,6 +0,0 @@
-class Deletecolumnsfrommemberships < ActiveRecord::Migration
-  def change
-    remove_column :memberships, :upload_count
-    remove_column :memberships, :latest_upload_date
-  end
-end
