ae4458f7af16493be950697a039d923a5158822e
Mike Knepper <michaelrknepper@gmail.com>
2013-09-24 12:52:54 -0500

    Changed pirate helper to use map --skip-ci

--- app/helpers/pirates_helper.rb
+++ app/helpers/pirates_helper.rb
@@ -4,15 +4,24 @@ module PiratesHelper
   end
 
   def activations_y_values(year)
-    final_array = []
-    12.times do |count|
-      month = count + 1
-      target_month = (month + 1).to_s
-      date = "#{year} - #{target_month} - 1"
-      sql = "SELECT COUNT( * ) FROM (SELECT DISTINCT user_id FROM (SELECT user_id, DATE FROM of_of_measurements WHERE DATE < #{date}) AS uploads_to_date) AS distinct_uploads"
-      y_value = ActiveRecord::Base.connection.execute(sql).first
-      final_array << [month, y_value]
-    end
-    return final_array.to_json
+    (1..12).map do |month|
+      date = "#{year}/#{month + 1}/1"
+      sql = "SELECT COUNT( * ) FROM (SELECT DISTINCT user_id FROM (SELECT user_id, DATE FROM of_of_measurements WHERE DATE < '#{date}') AS uploads_to_date) AS distinct_uploads"
+      y_value = ActiveRecord::Base.connection.execute(sql)
+      [month, y_value]
+    end.to_json
   end
+
+  # def activations_y_values(year)
+  #   final_array = []
+  #   12.times do |count|
+  #     month = count + 1
+  #     target_month = (month + 1).to_s
+  #     date = "#{year} - #{target_month} - 1"
+      # sql = "SELECT COUNT( * ) FROM (SELECT DISTINCT user_id FROM (SELECT user_id, DATE FROM of_of_measurements WHERE DATE < #{date}) AS uploads_to_date) AS distinct_uploads"
+      # y_value = ActiveRecord::Base.connection.execute(sql).first
+  #     final_array << [month, y_value]
+  #   end
+  #   return final_array.to_json
+  # end
 end
