8af33cf6fe36abfd628ed9b847cdd1988f8cc658
Mike Knepper <michaelrknepper@gmail.com>
2013-09-25 14:10:26 -0500

    converting dates to date class explicitly; removed commented code --skip-ci

--- app/helpers/pirates_helper.rb
+++ app/helpers/pirates_helper.rb
@@ -6,11 +6,10 @@ module PiratesHelper
   def activations_y_values(year)
     (1..12).map do |month|
       if month < 12
-        date = "#{year}/#{month + 1}/01"
+        date = "#{year}/#{month + 1}/01".to_date
       else
-        date = "#{year + 1}/01/01"
+        date = "#{year + 1}/01/01".to_date
       end
-      # sql = "SELECT COUNT( * ) FROM (SELECT DISTINCT user_id FROM (SELECT user_id, DATE FROM of_of_measurements WHERE DATE < '#{date}') AS uploads_to_date) AS distinct_uploads"
       sql = "select count(distinct user_id) from of_of_measurements where date < '#{date}'"
       y_value = ActiveRecord::Base.connection.execute(sql)
       [month, y_value]
@@ -20,30 +19,6 @@ module PiratesHelper
   # def activations_y_values(year)
   #   sql = "SELECT user_id, date FROM of_of_measurements WHERE date < '#{year + 1}/01/01'"
   #   all_uploads_through_year = ActiveRecord::Base.connection.execute(sql)
-
-  #   (1..12).map do |month|
-  #     if month < 12
-  #       all_uploads_through_month = all_uploads_through_year.reject { |obj| obj[1] == nil || obj[1] >= "#{year}/#{month + 1}/01" }
-  #     else
-  #       all_uploads_through_month = all_uploads_through_year.reject { |obj| obj[1] == nil || obj[1] >= "#{year+1}/01/01" }
-  #     end
-  #     user_ids = []
-  #     all_uploads_through_month.each do |upload|
-  #       user_ids << upload[0]
-  #     end
-  #     unique_user_ids = user_ids.uniq
-  #     y_value = unique_user_ids.length
-  #     [month, y_value]
-  #   end.to_json
-  # end
-
-  # def activations_y_values(year)
-  #   # sql = "SELECT user_id, date FROM of_of_measurements WHERE date < '#{year + 1}/01/01'"
-  #   # all_uploads_through_year = ActiveRecord::Base.connection.execute(sql)
-
-
-  #   next_year = "#{year + 1}/01/01".to_date
-  #   all_uploads_through_year = Upload.select(user_id, date).where(:date < next_year)
 
   #   (1..12).map do |month|
   #     if month < 12
