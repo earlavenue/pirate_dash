00f1152d5893bdde53c72354e39cd9c9bda63d04
Mike Knepper <michaelrknepper@gmail.com>
2013-09-26 10:54:46 -0500

    yvalue sql query uses rails, not activerecord connection execute --skip-ci

--- app/helpers/pirates_helper.rb
+++ app/helpers/pirates_helper.rb
@@ -10,30 +10,9 @@ module PiratesHelper
       else
         date = "#{year + 1}/01/01".to_date
       end
-      sql = "select count(distinct user_id) from of_of_measurements where date < '#{date}'"
-      y_value = ActiveRecord::Base.connection.execute(sql)
+      y_value = Upload.select("count(distinct user_id) as foo").where("date < ?", date).first.foo
       [month, y_value]
     end.to_json
   end
-
-  # def activations_y_values(year)
-  #   sql = "SELECT user_id, date FROM of_of_measurements WHERE date < '#{year + 1}/01/01'"
-  #   all_uploads_through_year = ActiveRecord::Base.connection.execute(sql)
-
-  #   (1..12).map do |month|
-  #     if month < 12
-  #       all_uploads_through_month = all_uploads_through_year.reject { |obj| obj[1] == nil || obj[1] >= "#{year}/#{month + 1}/01" }
-  #     else
-  #       all_uploads_through_month = all_uploads_through_year.reject { |obj| obj[1] == nil || obj[1] >= "#{year+1}/01/01" }
-  #     end
-  #     user_ids = []
-  #     all_uploads_through_month.each do |upload|
-  #       user_ids << upload[0]
-  #     end
-  #     unique_user_ids = user_ids.uniq
-  #     y_value = unique_user_ids.length
-  #     [month, y_value]
-  #   end.to_json
-  # end
 
 end
