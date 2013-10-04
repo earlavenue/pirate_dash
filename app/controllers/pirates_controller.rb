b2c6d7cf82203884823b53df0c3b9d29df475e9d
Will Crouse <wcrouse72788@gmail.com>
2013-10-04 14:08:12 -0500

    Only querying one device's quarterly values to test --skip-ci

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -20,15 +20,16 @@ class PiratesController < ApplicationController
     @quarterly_values_HJ72 = quarterly_device_values(@month_count_for_device_HJ72)
     @device_HJ72 = ['HJ-720IT', @quarterly_values_HJ72]
 
-    @month_count_for_device_HJA = month_count_for_device(2013, "HJA")
-    @quarterly_values_HJA = quarterly_device_values(@month_count_for_device_HJA)
-    @device_HJA = ['HJA-312', @quarterly_values_HJA]
+    # @month_count_for_device_HJA = month_count_for_device(2013, "HJA")
+    # @quarterly_values_HJA = quarterly_device_values(@month_count_for_device_HJA)
+    # @device_HJA = ['HJA-312', @quarterly_values_HJA]
 
 
-    @month_count_for_device_002 = month_count_for_device(2013, "002")
-    @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
-    @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
+    # @month_count_for_device_002 = month_count_for_device(2013, "002")
+    # @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
+    # @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
 
-    @devices = [@device_HJ72, @device_HJA, @device_002]
+    # @devices = [@device_HJ72, @device_HJA, @device_002]
+    @devices = [@device_HJ72]
   end
 end
