10f78801cf15d3596804d6e1a77f5e3de80578d5
Will Crouse <wcrouse72788@gmail.com>
2013-10-04 22:28:01 -0500

    Quarterly device display worked with 2 devices. Trying 3. --skip-ci

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -22,12 +22,12 @@ class PiratesController < ApplicationController
     @quarter_count_for_HJA = quarter_count_for_device(2013, "HJA")
     @device_HJA = ['HJA-312', @quarter_count_for_HJA]
 
-
+    @quarter_count_for_002 = quarter_count_for_device(2013, "002")
+    @device_002 = ['HJ-322, 323, 324', @quarter_count_for_002]
     # @month_count_for_device_002 = month_count_for_device(2013, "002")
     # @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
     # @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
 
-    # @devices = [@device_HJ72, @device_HJA, @device_002]
-    @devices = [@device_HJ72, @device_HJA]
+    @devices = [@device_HJ72, @device_HJA, @device_002]
   end
 end
