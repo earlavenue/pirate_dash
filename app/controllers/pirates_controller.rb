a09c70f491b921a8d05ba95ef572f7d3b02b5b9d
Mike Knepper <michaelrknepper@gmail.com>
2013-10-07 09:47:13 -0500

    Fixed 0002 prefix --skip-ci

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -22,10 +22,10 @@ class PiratesController < ApplicationController
     @quarter_count_for_HJA = quarter_count_for_device(2013, "HJA")
     @device_HJA = ['HJA-312', @quarter_count_for_HJA]
 
-    @quarter_count_for_002 = quarter_count_for_device(2013, "002")
-    @device_002 = ['HJ-322, 323, 324', @quarter_count_for_002]
+    @quarter_count_for_0002 = quarter_count_for_device(2013, "0002")
+    @device_0002 = ['HJ-322, 323, 324', @quarter_count_for_0002]
 
-   @devices = [@device_HJ72, @device_HJA, @device_002]
+    @devices = [@device_HJ72, @device_HJA, @device_0002]
     # @month_count_for_device_002 = montht_count_for_device(2013, "002")
     # @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
     # @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
