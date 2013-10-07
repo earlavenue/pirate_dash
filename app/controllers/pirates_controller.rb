9f1bd4e8f613460747505339ff38d01fa6711126
Will Crouse <wcrouse72788@gmail.com>
2013-10-07 09:17:05 -0500

    Switched month iteration to quarter iteration for devices --skip-ci

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -24,10 +24,10 @@ class PiratesController < ApplicationController
 
     @quarter_count_for_002 = quarter_count_for_device(2013, "002")
     @device_002 = ['HJ-322, 323, 324', @quarter_count_for_002]
-    # @month_count_for_device_002 = month_count_for_device(2013, "002")
+
+   @devices = [@device_HJ72, @device_HJA, @device_002]
+    # @month_count_for_device_002 = montht_count_for_device(2013, "002")
     # @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
     # @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
-
-    @devices = [@device_HJ72, @device_HJA, @device_002]
   end
 end
