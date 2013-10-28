3f760e21a463c461e7130bc3aee7588797133bd1
Mike Knepper <michaelrknepper@gmail.com>
2013-10-28 14:32:09 -0500

    Added whenever to handle cron jobs; previous quarter personquarter rake task

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -1,7 +1,6 @@
  class PiratesController < ApplicationController
   before_action :require_signin
   before_action :require_omron_business
-  before_action :check_and_create_person_quarters, only: [:retention]
 
   include PiratesHelper
 
@@ -119,65 +118,4 @@
     @start_date = params[:quarter]
     @person_quarters = PersonQuarter.users_lost(@start_date)
   end
-
-
-  private
-
-  def check_and_create_person_quarters
-    if params[:year]
-      @year = params[:year].to_i
-    else
-      @year = Time.now.year
-    end
-
-    @q1_start = "#{@year}-01-01".to_date
-    @q2_start = "#{@year}-04-01".to_date
-    @q3_start = "#{@year}-07-01".to_date
-    @q4_start = "#{@year}-10-01".to_date
-
-    if @year == Time.now.year
-      current_month = Time.now.month
-
-      quarter_hash = { 1 => @q1_start, 2 => @q1_start, 3 => @q1_start, 4 => @q2_start, 5 => @q2_start, 6 => @q2_start, 7 => @q3_start, 8 => @q3_start, 9 => @q3_start, 10 => @q4_start, 11 => @q4_start, 12 => @q4_start }
-
-      current_quarter = quarter_hash[current_month]
-      previous_quarter = current_quarter - 3.months
-
-      unless PersonQuarter.where(start_date: previous_quarter).present?
-
-        # create PQ records for everyone who has uploaded at all
-        all_users_who_have_uploaded = Upload.select("distinct user_id")
-        all_users_who_have_uploaded.each do |row|
-          pq = PersonQuarter.new
-          pq.person_id = row.user_id
-          pq.active = false
-          pq.start_date = previous_quarter
-
-          # set the "previous quarter" boolean -- note: relative to Time.now, this is referring to TWO quarters ago
-          two_quarters_ago_start_date = previous_quarter - 3.months
-          previous_person_quarter = PersonQuarter.where(person_id: row.user_id).where(start_date: two_quarters_ago_start_date).first
-          if previous_person_quarter
-            if previous_person_quarter.active == false
-              pq.previous_quarter = false
-            else
-              pq.previous_quarter = true
-            end
-          else
-            pq.previous_quarter = false
-          end
-
-          pq.save
-        end
-
-        # edit the PQs for users who were active (15+ uploads) during the previous quarter
-        active_users_in_previous_quarter = Upload.find_by_sql(["select user_id, count(*) from of_of_measurements where date >= ? and date < ? group by user_id having count(*) > 14", previous_quarter, current_quarter])
-        active_users_in_previous_quarter.each do |row|
-          q = PersonQuarter.where("person_id = ? and start_date = ?", row.user_id, previous_quarter).first
-          q.active = true
-          q.save
-        end
-      end
-    end
-  end
-
 end
