5377de533a353a5680df2639724d4c1bf2b243e6
Mike Knepper <michaelrknepper@gmail.com>
2013-10-28 13:45:28 -0500

    Before action to create person_quarters if necessary

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -1,6 +1,7 @@
  class PiratesController < ApplicationController
   before_action :require_signin
   before_action :require_omron_business
+  before_action :check_and_create_person_quarters, only: [:retention]
 
   include PiratesHelper
 
@@ -119,25 +120,64 @@
     @person_quarters = PersonQuarter.users_lost(@start_date)
   end
 
+
+  private
+
+  def check_and_create_person_quarters
+    if params[:year]
+      @year = params[:year].to_i
+    else
+      @year = Time.now.year
+    end
+
+    @q1_start = "#{@year}-01-01".to_date
+    @q2_start = "#{@year}-04-01".to_date
+    @q3_start = "#{@year}-07-01".to_date
+    @q4_start = "#{@year}-10-01".to_date
+
+    if @year == Time.now.year
+      current_month = Time.now.month
+
+      quarter_hash = { 1 => @q1_start, 2 => @q1_start, 3 => @q1_start, 4 => @q2_start, 5 => @q2_start, 6 => @q2_start, 7 => @q3_start, 8 => @q3_start, 9 => @q3_start, 10 => @q4_start, 11 => @q4_start, 12 => @q4_start }
+
+      current_quarter = quarter_hash[current_month]
+      previous_quarter = current_quarter - 3.months
+
+      unless PersonQuarter.where(start_date: previous_quarter).present?
+
+        # create PQ records for everyone who has uploaded at all
+        all_users_who_have_uploaded = Upload.select("distinct user_id")
+        all_users_who_have_uploaded.each do |row|
+          pq = PersonQuarter.new
+          pq.person_id = row.user_id
+          pq.active = false
+          pq.start_date = previous_quarter
+
+          # set the "previous quarter" boolean -- note: relative to Time.now, this is referring to TWO quarters ago
+          two_quarters_ago_start_date = previous_quarter - 3.months
+          previous_person_quarter = PersonQuarter.where(person_id: row.user_id).where(start_date: two_quarters_ago_start_date).first
+          if previous_person_quarter
+            if previous_person_quarter.active == false
+              pq.previous_quarter = false
+            else
+              pq.previous_quarter = true
+            end
+          else
+            pq.previous_quarter = false
+          end
+
+          pq.save
+        end
+
+        # edit the PQs for users who were active (15+ uploads) during the previous quarter
+        active_users_in_previous_quarter = Upload.find_by_sql(["select user_id, count(*) from of_of_measurements where date >= ? and date < ? group by user_id having count(*) > 14", previous_quarter, current_quarter])
+        active_users_in_previous_quarter.each do |row|
+          q = PersonQuarter.where("person_id = ? and start_date = ?", row.user_id, previous_quarter).first
+          q.active = true
+          q.save
+        end
+      end
+    end
+  end
+
 end
-
-
-    # @quarter_count_for_HJ72 = quarter_count_for_device(@year, "HJ-720")
-    # @device_HJ72 = ['HJ-720IT', @quarter_count_for_HJ72]
-
-    # @quarter_count_for_HJA = quarter_count_for_device(@year, "HJA-312")
-    # @device_HJA = ['HJA-312', @quarter_count_for_HJA]
-
-    # @quarter_count_for_HJ322 = quarter_count_for_device(@year, "HJ-322")
-    # @device_HJ322 = ['HJ-322', @quarter_count_for_HJ322]
-
-    # @quarter_count_for_HJ323 = quarter_count_for_device(@year, "HJ-323")
-    # @device_HJ323 = ['HJ-323', @quarter_count_for_HJ323]
-
-    # @device_HJ320s = ['HJ-320s', @quarter_count_for_HJ322 + @quarter_count_for_HJ323]
-
-    # @devices = [@device_HJ72, @device_HJA, @device_HJ320s]
-    # @month_count_for_device_002 = montht_count_for_device(2013, "002")
-    # @quarterly_values_002 = quarterly_device_values(@month_count_for_device_002)
-    # @device_002 = ['HJ-322, 323, 324', @quarterly_values_002]
-
