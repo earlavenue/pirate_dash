2f519e4a4f857438165d2faa1a755b685caf22be
Mike Knepper <michaelrknepper@gmail.com>
2013-11-11 14:38:49 -0600

    Devices action simplified --skip-ci

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -15,6 +15,7 @@
     @y_values_as_json = @aggregate_month_activations.to_json
   end
 
+
   def devices
     if params[:year]
       @year = params[:year].to_i
@@ -22,89 +23,28 @@
       @year = Time.now.year
     end
 
-    @q1_start = "#{@year}-01-01".to_date
-    @q2_start = "#{@year}-04-01".to_date
-    @q3_start = "#{@year}-07-01".to_date
-    @q4_start = "#{@year}-10-01".to_date
+    current_quarter = Time.now.beginning_of_quarter.to_date
 
-    if @year == Time.now.year
-      current_month = Time.now.month
-
-      quarter_hash = { 1 => @q1_start, 2 => @q1_start, 3 => @q1_start, 4 => @q2_start, 5 => @q2_start, 6 => @q2_start, 7 => @q3_start, 8 => @q3_start, 9 => @q3_start, 10 => @q4_start, 11 => @q4_start, 12 => @q4_start }
-
-      current_quarter = quarter_hash[current_month]
-
-      if current_quarter == "#{@year}-01-01".to_date
-        @Q1HJ720 = Pirate.current_quarter_activations("HJ-720", current_quarter)
-        @Q1HJA312 = Pirate.current_quarter_activations("HJA-312", current_quarter)
-        @Q1HJ322 = Pirate.current_quarter_activations("HJ-322", current_quarter)
-        @Q2HJ720 = "-"
-        @Q2HJA312 = "-"
-        @Q2HJ322 = "-"
-        @Q3HJ720 = "-"
-        @Q3HJA312 = "-"
-        @Q3HJ322 = "-"
-        @Q4HJ720 = "-"
-        @Q4HJA312 = "-"
-        @Q4HJ322 = "-"
-      elsif current_quarter == "#{@year}-04-01".to_date
-        @Q1HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q1_start).first.activations
-        @Q1HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q1_start).first.activations
-        @Q1HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q1_start).first.activations
-        @Q2HJ720 = Pirate.current_quarter_activations("HJ-720", current_quarter)
-        @Q2HJA312 = Pirate.current_quarter_activations("HJA-312", current_quarter)
-        @Q2HJ322 = Pirate.current_quarter_activations("HJ-322", current_quarter)
-        @Q3HJ720 = "-"
-        @Q3HJA312 = "-"
-        @Q3HJ322 = "-"
-        @Q4HJ720 = "-"
-        @Q4HJA312 = "-"
-        @Q4HJ322 = "-"
-      elsif current_quarter == "#{@year}-07-01".to_date
-        @Q1HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q1_start).first.activations
-        @Q1HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q1_start).first.activations
-        @Q1HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q1_start).first.activations
-        @Q2HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q2_start).first.activations
-        @Q2HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q2_start).first.activations
-        @Q2HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q2_start).first.activations
-        @Q3HJ720 = Pirate.current_quarter_activations("HJ-720", current_quarter)
-        @Q3HJA312 = Pirate.current_quarter_activations("HJA-312", current_quarter)
-        @Q3HJ322 = Pirate.current_quarter_activations("HJ-322", current_quarter)
-        @Q4HJ720 = "-"
-        @Q4HJA312 = "-"
-        @Q4HJ322 = "-"
-      elsif current_quarter == "#{@year}-10-01".to_date
-        @Q1HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q1_start).first.activations
-        @Q1HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q1_start).first.activations
-        @Q1HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q1_start).first.activations
-        @Q2HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q2_start).first.activations
-        @Q2HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q2_start).first.activations
-        @Q2HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q2_start).first.activations
-        @Q3HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q3_start).first.activations
-        @Q3HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q3_start).first.activations
-        @Q3HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q3_start).first.activations
-        @Q4HJ720 = Pirate.current_quarter_activations("HJ-720", current_quarter)
-        @Q4HJA312 = Pirate.current_quarter_activations("HJA-312", current_quarter)
-        @Q4HJ322 = Pirate.current_quarter_activations("HJ-322", current_quarter)
+    @device_hash = Hash.new
+    date = "#{@year}-01-01".to_date
+    4.times do |i|
+      if current_quarter == date
+        @device_hash["HJ-720_#{date}"] = Pirate.current_quarter_activations("HJ-720", current_quarter)
+        @device_hash["HJ-322_#{date}"] = Pirate.current_quarter_activations("HJ-322", current_quarter)
+        @device_hash["HJA-312_#{date}"] = Pirate.current_quarter_activations("HJA-312", current_quarter)
+      elsif current_quarter < date
+        @device_hash["HJ-720_#{date}"] = "-"
+        @device_hash["HJ-322_#{date}"] = "-"
+        @device_hash["HJA-312_#{date}"] = "-"
+      else
+        @device_hash["HJ-720_#{date}"] = Rails.cache.read("HJ-720_#{date}")
+        @device_hash["HJ-322_#{date}"] = Rails.cache.read("HJ-322_#{date}")
+        @device_hash["HJA-312_#{date}"] = Rails.cache.read("HJA-312_#{date}")
       end
-    else
-      @Q1HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q1_start).first.activations
-      @Q2HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q2_start).first.activations
-      @Q3HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q3_start).first.activations
-      @Q4HJ720 = DeviceQuarter.where(device_model: "HJ-720").where(start_date: @q4_start).first.activations
-
-      @Q1HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q1_start).first.activations
-      @Q2HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q2_start).first.activations
-      @Q3HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q3_start).first.activations
-      @Q4HJA312 = DeviceQuarter.where(device_model: "HJA-312").where(start_date: @q4_start).first.activations
-
-      @Q1HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q1_start).first.activations
-      @Q2HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q2_start).first.activations
-      @Q3HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q3_start).first.activations
-      @Q4HJ322 = DeviceQuarter.where(device_model: "HJ-322").where(start_date: @q4_start).first.activations
+      date += 3.months
     end
-
   end
+
 
   def retention
     if params[:year]
@@ -113,6 +53,7 @@
       @year = Time.now.year
     end
   end
+
 
   def users_lost
     @start_date = params[:quarter]
