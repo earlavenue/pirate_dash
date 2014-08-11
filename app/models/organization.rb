d5e712d6680017f9373f1ea283cb8720ca364958
Will Crouse <wcrouse72788@gmail.com>
2014-08-10 21:54:26 -0500

    finding size of CSV

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -11,16 +11,18 @@ class Organization < ActiveRecord::Base
   has_many :clients
 
   def export
-    # csv_string = CSV.generate do |csv|
-    #   csv << ["Name", "Upload Date", "Steps", "Aerobic Steps", "Calories", "Miles", "Device Serial", "Input Method"]
+    csv_string = CSV.generate do |csv|
+      csv << ["Name", "Upload Date", "Steps", "Aerobic Steps", "Calories", "Miles", "Device Serial", "Input Method"]
       self.uploads.includes(:person).find_each(batch_size: 6000) do |upload|
         if upload.is_device_input == 1
           input_method = "Synced from Device"
         else
           input_method = "Manually Uploaded"
         end
-        # csv << ["#{upload.person.first_name} #{upload.person.last_name}", upload.date.try(:strftime, "%b %d %Y"), upload.total_steps, upload.aerobic_steps, upload.calories, ('%.2f' % upload.distance), upload.device_serial, input_method]
+        csv << ["#{upload.person.first_name} #{upload.person.last_name}", upload.date.try(:strftime, "%b %d %Y"), upload.total_steps, upload.aerobic_steps, upload.calories, ('%.2f' % upload.distance), upload.device_serial, input_method]
       end
-    # end
+    require 'objspace'
+    Rails.logger.debug "CSV is this size: #{ObjectSpace.memsize_of(csv)}"
+    end
   end
 end
