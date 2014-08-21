1c3d74dea966cc019915b6a5a66e80150106f865
Will Crouse <wcrouse72788@gmail.com>
2014-08-20 23:22:27 -0500

    set the hirefire environment

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,5 +1,5 @@
 HireFire.configure do |config|
-  config.environment      = nil # default in production is :heroku. default in development is :noop
+  config.environment      = :heroku # default in production is :heroku. default in development is :noop
   config.max_workers      = 2   # default is 1
   config.min_workers      = 0   # default is 0
   config.job_worker_ratio = [
